8a2826a50651571467691feea846c1f70d86d311
Abhyudaya Agrawal <128376928+abhyudaya3@users.noreply.github.com>
2024-01-07 17:24:33 +0000

    new file added:- variable.js

---
+++ 01_basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 1345;
+let accountEmail = "bro@gmail.com";
+var accountPassword = "123"; //Scoping Problem
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2; cant write it in this format;
+
+console.log(accountEmail);
+console.table([accountCity, accountPassword, accountEmail, accountState]);
