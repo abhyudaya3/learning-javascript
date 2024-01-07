bc50325ffdea7113203770e75e31a40596dcce53
Abhyudaya Agrawal <128376928+abhyudaya3@users.noreply.github.com>
2024-01-07 17:55:13 +0000

    added file:- -2_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3); //we are using Node.js not browser
+
+let name = 'abhy';
+let age =23;
+let isLoggedIn = false;
+
+// number 2to the power 53
+// bigint
+// string
+// bollean
+// null => standalone value its ****type is Object
+// undefined
+// symbol 
+// object
+
+console.log(typeof null); // prints object
+console.log(typeof undefined)
